3c52b1e108994a61dcdcc9ffc63ad8d841fa13f6
jef <jef@codewerken.be>
2022-04-06 02:03:46 +0200

    a simple websocket+backend test

--- frontend/src/components/Test.vue
+++ frontend/src/components/Test.vue
@@ -1,7 +1,7 @@
 <!-- vim: set ts=2 sw=2 expandtab : -->
 <template>
   <div>
-  <button @click="clicked">count is: {{ count }}</button>
+  <button @click="clicked">push something to every client</button>
   <ul>
     <li v-for="(tag, tag_index) in tags" :key="tag_index">
       {{ tag }}
@@ -15,20 +15,18 @@ export default {
   name : 'Test',
   data : function() {
     return {
-      count : 0,
       tags : [],
     };
   },
   sockets: {
     new_tag : function(data) {
       console.log('new_tag(data='+data+')');
-      this.tags.push(data);
+      this.tags.push(`server just said "${data}"`);
     },
   },
   methods : {
     clicked() {
       this.$socket.emit('debug', 'new_tag');
-      this.count++;
     }
   }
 };
