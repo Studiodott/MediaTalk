274d16e36f228889448d02d158c9a908ccbf388d
jef <jef@codewerken.be>
2022-05-26 02:10:09 +0200

    uniform but ugly way of distributing selections/highlights

--- frontend/src/components/Media.vue
+++ frontend/src/components/Media.vue
@@ -7,8 +7,9 @@
       <div
         v-if="media_type == 'VIDEO'">
         <MediaVideo
-          @advanced="advanced"
-          :highlight="highlight"
+          @advanced="advanced_by_media"
+          :highlights="highlights_for_media"
+          :selection="selection_for_media"
           :src="url_original"
           :waveform="url_description"/>
       </div>
@@ -26,13 +27,15 @@
       <div
         v-else-if="media_type == 'IMAGE'">
         <MediaImage
-          :highlight="highlight"
-          @selected="media_selected"
+          :highlights="highlights_for_media"
+          @selected="selected_by_media"
           v-bind:src="url_original"/>
       </div>
       <div
         v-else-if="media_type == 'TEXT'">
         <MediaText
+          :highlights="highlights_for_media"
+          @selected="selected_by_media"
           v-bind:src="url_original"/>
       </div>
     </div>
@@ -41,25 +44,29 @@
       <!--
       <TagChooserTimeline
         :selection="selection"
-        :last_advance="last_advance"
+        :advance_for_tagchooser="advance_for_tagchooser"
         :media_handle="handle"/>
       -->
       <TagChooserTimeline
         v-if="media_type == 'VIDEO' || media_type == 'AUDIO'"
-        :last_advance="last_advance"
+        :advance="advance_for_tagchooser"
+        @selected="selected_by_tagchooser"
         :media_handle="handle"/>
       <TagChooserStatic
         v-if="media_type == 'TEXT' || media_type == 'IMAGE'"
-        :selection="selection"
+        :selection="selection_for_tagchooser"
         :media_handle="handle"/>
       <p>Existing tags:</p>
       <ul>
         <li
           v-for="(tagging_positions, tag_name) in getTagsForMedia">
-          {{ tag_name }}
+          <a
+            @click="highlight_taggings(tagging_positions)">
+            {{ tag_name }}
+          </a>
           <ul>
             <li v-for="p in tagging_positions">
-              <a @click="highlight_tagging(p)">here</a>
+              <a @click="highlight_taggings([ p ])">here</a>
             </li>
           </ul><!--<PositionDisplay v-for="p in tagging_positions" :position="p" />-->
         </li>
@@ -90,9 +97,10 @@ export default {
   name : 'Media',
   data : function() {
     return {
-      selection : undefined,
-      highlight : undefined,
-      last_advance : undefined,
+      selection_for_tagchooser : undefined,
+      selection_for_media : undefined,
+      highlights_for_media : undefined,
+      advance_for_tagchooser : undefined,
     };
   },
   components : {
@@ -117,20 +125,23 @@ export default {
     'url_original',
     'url_description',
   ],
-  mounted : function() {
-    console.log("mounted");
-  },
   methods : {
-    advanced : function(p) {
-      this.last_advance = p;
+    // the media called a time advancement, propagate to tagchooser
+    advanced_by_media : function(p) {
+      this.advance_for_tagchooser = p;
     },
-    media_selected(selection) {
+    // the media called a selection, propagate to tagchooser
+    selected_by_media(selection) {
       console.log(selection);
-      this.selection = selection;
+      this.selection_for_tagchooser = selection;
     },
-    highlight_tagging : function(p) {
-      console.log(p);
-      this.highlight = p;
+    // the tagchooser called a selection, propagate to media
+    selected_by_tagchooser(selection) {
+      this.selection_for_media = selection;
+    },
+    // we ourselves want a tagging list highlighted, propagate to media
+    highlight_taggings : function(l) {
+      this.highlights_for_media = l;
     },
   },
   computed : {
@@ -144,7 +155,6 @@ export default {
           if (!(tag.name in as_dict)) {
             as_dict[tag.name] = [];
           }
-          console.log(ti.position);
           as_dict[tag.name].push(JSON.parse(ti.position));
         }
       });
